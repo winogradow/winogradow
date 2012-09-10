d293e85ad81002bdb970d72b770ff06e030942e1
vinogradov <winogradow@bigmir.net>
2012-09-10 23:51:33 +0300

    111

---
+++ dirs&files/index.php
@@ -0,0 +1,49 @@
+<style>
+.dir {
+color: black;
+}
+.file {
+color: green;
+}
+</style>
+<?php 
+
+function verifyDir($folder)
+ {
+  if(is_string ( $folder )) {
+   if(is_dir($folder)) {
+     return 1;
+    } else {
+     if(fopen($folder, "r")) throw new Exception('Путь является файлом') ;
+      else throw new Exception('Такого пути не существует'); 
+     }
+   } else throw new Exception('Путь задан неверно');
+  return 1;
+ }
+ 
+function showdir($folder) 
+ { 
+ $files = scandir($folder); 
+ echo '<ul>';
+ foreach($files as $file) { 
+  if ($file[0]=='.') continue; 
+  $fPath=$folder.'/'.$file;
+  if (is_dir($fPath)) { 
+   echo '<li class="dir">'.$file.'</li>'."\n"; 
+   showdir($fPath); 
+  } else {
+   echo '<li class="file">'.$file.'</li>'."\n"; 
+   }
+ } 
+ echo '</ul>';
+} 
+ 
+
+$dir='./';
+
+
+if(  verifyDir($dir)) {
+    showdir($dir); 
+ }
+
+ 
